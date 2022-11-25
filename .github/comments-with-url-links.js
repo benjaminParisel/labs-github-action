017ec033dcd6b2bea3b0c0eda2424fddd6cffcb2
benjaminParisel <benjamin.parisel@bonitasoft.com>
2022-11-25 15:31:05 +0100

    add deleted link

--- .github/comments-with-url-links.js
+++ .github/comments-with-url-links.js
@@ -1,50 +1,56 @@
 const githubUtils = require('./github');
-const template= '<!-- previewLinksCheck-->\n';
+const template = '<!-- previewLinksCheck-->\n';
 module.exports = {
-    prepareUrlLinks:  async function ({github, context}) {
-            let {FILES, SITE_URL, COMPONENT_NAME} = process.env;
-            const {data: pr} = await github.rest.pulls.get({
-                owner: context.repo.owner,
-                pull_number: context.issue.number,
-                repo: context.repo.repo,
-            });
-            let files = FILES.split(' ');
-            let urls = [];
-            files.forEach(file => {
-                const splitted = file.split('/');
-                splitted.shift();
-                const pageName = splitted.pop();
-                const moduleName = splitted.shift();
-                let url = `${SITE_URL}/${COMPONENT_NAME}/${pr.base.ref}${moduleName === 'ROOT' ? '/' : `/${moduleName}/`}${pageName?.split('.').shift()}`;
-                urls.push(`- [ ] [${moduleName}/${pageName}](${url})`);
-            });
-            return urls.join('\n');
+    prepareUrlLinks: async function ({github, context}) {
+        let {FILES,DELETED, SITE_URL, COMPONENT_NAME} = process.env;
+        const {data: pr} = await github.rest.pulls.get({
+            owner: context.repo.owner,
+            pull_number: context.issue.number,
+            repo: context.repo.repo,
+        });
+        let res = {};
+        res.updated = prepareLinks({files: FILES.split(' '), siteUrl: SITE_URL, component: COMPONENT_NAME, branch: pr.base.ref});
+        res.deleted = prepareLinks({files: DELETED.split(' '), siteUrl: SITE_URL, component: COMPONENT_NAME, branch: pr.base.ref});
+        return res;
     },
-    createOrUpdateComments: async function ({github,context}){
+    createOrUpdateComments: async function ({github, context}) {
         let {LINKS, RENAMED_FILES, HAS_DELETED_FILES} = process.env;
-        const header='## :memo: Check the pages that have been modified\n\n';
-        let newBody = buildMessage(header,LINKS,HAS_DELETED_FILES === 'true' || RENAMED_FILES != '');
-        const {exists, id, body} = await githubUtils.isCommentExist({github,context,template: template});
+        const header = '## :memo: Check the pages that have been modified\n\n';
+        let newBody = buildMessage({header, links: JSON.parse(LINKS),hasWarnigMessage : HAS_DELETED_FILES === 'true' || RENAMED_FILES != ''});
+        const {exists, id, body} = await githubUtils.isCommentExist({github, context, template: template});
         // Delete oldest comment if another comments exist
-        if (exists && id && body === newBody){
-            await githubUtils.updateComment({github,context,comment_id: id, body: newBody});
+        if (exists && id && body === newBody) {
+            await githubUtils.updateComment({github, context, comment_id: id, body: newBody});
             return id;
         }
-        const comment = await githubUtils.createComment({github,context,body: newBody});
+        const comment = await githubUtils.createComment({github, context, body: newBody});
         return comment?.id;
     }
 };
 
-function buildMessage(header,links,hasWarningMessage){
+function buildMessage({header, links, deletedLinks, hasWarningMessage}) {
     const preface =
         'In order to merge this pull request, you need to check your updates with the following url.\n\n';
 
-    const availableLinks = `### :mag: Page list: \n ${links}\n\n\n\n`;
+    const availableLinks = `### :mag: Page list: \n ${links.updated}\n\n\n\n`;
     //Adding deleted or renamed check
     let warningAliasMessage = '';
-    if(hasWarningMessage){
-        warningAliasMessage='\n \n ### :warning: At least one page has been deleted in the Pull Request. Make sure to add [aliases](https://github.com/bonitasoft/bonita-documentation-site/blob/master/docs/content/CONTRIBUTING.adoc#use-alias-to-create-redirects)'
+    if (hasWarningMessage) {
+        warningAliasMessage = `\n \n ### :warning: At least one page has been deleted in the Pull Request. Make sure to add [aliases](https://github.com/bonitasoft/bonita-documentation-site/blob/master/docs/content/CONTRIBUTING.adoc#use-alias-to-create-redirects) \n ${links?.deleted} \n`
     }
 
     return template + header + preface + availableLinks + warningAliasMessage;
+}
+
+function prepareLinks({files, siteUrl, component, branch}) {
+    let urls = [];
+    files.forEach(file => {
+        const splitted = file.split('/');
+        splitted.shift();
+        const pageName = splitted.pop();
+        const moduleName = splitted.shift();
+        let url = `${siteUrl}/${component}/${branch}${moduleName === 'ROOT' ? '/' : `/${moduleName}/`}${pageName?.split('.').shift()}`;
+        urls.push(`- [ ] [${moduleName}/${pageName}](${url})`);
+    });
+    return urls.join('\n');
 }
